9f18a3654e96800725a14212678c04f0fd813cb3
LuthfiDaffa <126976260+LuthfiDaffa@users.noreply.github.com>
2023-03-08 14:25:24 +0700

    Menambahkan file identitas

---
+++ vario_syauqi.py
@@ -0,0 +1,7 @@
+nama_depan = "Muhammad Yanto"
+nama_belakang = "Petrus Shidarta"
+
+print(nama_depan, nama_belakang)
+print("Namaku" + nama_depan)
+print("syauqi ketilang mampus")
+print("Namaku" + nama_depan + "lhoo")
