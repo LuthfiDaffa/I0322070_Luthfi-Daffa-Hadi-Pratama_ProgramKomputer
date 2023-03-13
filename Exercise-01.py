0cde66f014399d2b710eac824d23d955fba96f47
Luthfi Daffa <luthfi.dhp@gmail.com>
2023-03-13 18:19:34 +0700

    Menambahkan Tugas Exercise01

---
+++ Exercise-01.py
@@ -0,0 +1,21 @@
+print ("===========================================================================================================================================================")
+
+a = "Nama = "
+b = "NIM = "
+c = "Kampung Halaman = "
+d = "Visualisasi = "
+e = "Harapan Setelah Lulus = "
+
+Nama = "Luthfi Daffa Hadi Pratama"
+NIM = "I0322070"
+KampungHalaman = "Bekasi"
+Visualiasasi = "Bagian depan rumahku ada masjid dan samping kanan rumahku ada taman bermain yang luas"
+HarapanSetelahLulus = "Harapan saya seterlah lulus dari Teknik Industri UNS adalah menjadi seorang entreprenur yang sukses dan dapat bekerja di perusahaan besar yang saya impikan"
+
+print(a + Nama)
+print(b + NIM)
+print(c + KampungHalaman)
+print(d + Visualiasasi)
+print(e + HarapanSetelahLulus)
+
+print ("===========================================================================================================================================================")
